543dd852c404966c6d6bba2d2befa844c0baba2a
Maru812 <driadesm8@gmail.com>
2025-07-31 01:07:28 -0400

    feat: add docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.12-slim-bookworm
+
+LABEL maintainer="The Five Hundred Band"
+
+# Install requirements
+RUN pip install poetry 
+
+COPY ./pyproject.toml ./poetry.lock* /tmp/
+
+RUN cd /tmp \
+    && poetry config virtualenvs.create false \
+    && poetry install --no-interaction --no-root
+
+WORKDIR /app
+COPY ./src /app
+
+EXPOSE 8000
+
+CMD sh -c "python manage.py migrate && python manage.py runserver 0.0.0.0:8000"
--- src/config/settings.py
+++ src/config/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(f"BASE_DIR {BASE_DIR}")
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
@@ -86,7 +85,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(default="postgresql://postgres:mysecretpassword@localhost:5433/youtb_clone"),
+    "default": env.db("DATABASE_URL", default="postgres://"),
+    # "default": env.db(default="postgresql://postgres:mysecretpassword@localhost:5433/youtb_clone"),
 }
 
 
